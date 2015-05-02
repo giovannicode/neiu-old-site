d9f893b1a1525aecb58805223daff2c498cb1337
drone-xb81 <giovannicode7@gmail.com>
2015-05-02 15:12:40 -0500

    Add original code from old neiu student affairs site.

---
+++ js/sswcaption.js
@@ -0,0 +1,37 @@
+﻿window.onload = initAll;
+
+var currImg = 0;
+var captionText = new Array(
+	"Welcome to the Division of Student Life Image Gallery!",
+	"next image"
+)
+
+function initAll() {
+	document.getElementById("imgText").innerHTML = captionText[0];
+	document.getElementById("prevLink").onclick = processPrevious;
+	document.getElementById("nextLink").onclick = processNext;
+}
+
+function processPrevious() {
+	newSlide(-1);
+}
+
+function processNext() {
+	newSlide(1);
+}
+
+function newSlide(direction) {
+	var imgCt = captionText.length;
+
+	currImg = currImg + direction;
+	if (currImg < 0) {
+		currImg = imgCt-1;
+	}
+	if (currImg == imgCt) {
+		currImg = 0;
+	}
+	document.getElementById("slideshow").src = "gallery/slideImg" + currImg + ".jpg";
+	document.getElementById("imgText").innerHTML = captionText[currImg];
+}
+
+						
